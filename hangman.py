6dea365a0f6911d8db5fb4081f7aafd138737e71
nishukee <nishchaymnayak@gmail.com>
2020-10-15 11:55:10 +0530

    Finished errors and bugs

--- hangman.py
+++ hangman.py
@@ -2,38 +2,41 @@ import random
 import time
 
 def main():
-    global count,display,word,already_guessed,length,play_game
+    global count,display,word,already_guessed,length,play_game,temp
     words_to_guess = ['abruptly', 'absurd', 'abyss', 'affix', 'askew', 'avenue', 'awkward', 'axiom', 'azure', 'bagpipes', 'bandwagon', 'banjo', 'bayou', 'beekeeper', 'bikini', 'blitz', 'blizzard', 'boggle', 'bookworm', 'boxcar', 'boxful', 'buckaroo', 'buffalo', 'buffoon', 'buxom', 'buzzard', 'buzzing', 'buzzwords', 'caliph', 'cobweb', 'cockiness', 'croquet', 'crypt', 'curacao', 'cycle', 'daiquiri', 'dirndl', 'disavow', 'dizzying', 'duplex', 'dwarves', 'embezzle', 'equip', 'espionage', 'euouae', 'exodus', 'faking', 'fishhook', 'fixable', 'fjord', 'flapjack', 'flopping', 'fluffiness', 'flyby', 'foxglove', 'frazzled', 'frizzled', 'fuchsia', 'funny', 'gabby', 'galaxy', 'galvanize', 'gazebo', 'giaour', 'gizmo', 'glowworm', 'glyph', 'gnarly', 'gnostic', 'gossip', 'grogginess', 'haiku', 'haphazard', 'hyphen', 'iatrogenic', 'icebox', 'injury', 'ivory', 'ivy', 'jackpot', 'jaundice', 'jawbreaker', 'jaywalk', 'jazziest', 'jazzy', 'jelly', 'jigsaw', 'jinx', 'jiujitsu', 'jockey', 'jogging', 'joking', 'jovial', 'joyful', 'juicy', 'jukebox', 'jumbo', 'kayak', 'kazoo', 'keyhole', 'khaki', 'kilobyte', 'kiosk', 'kitsch', 'kiwifruit', 'klutz', 'knapsack', 'larynx', 'lengths', 'lucky', 'luxury', 'lymph', 'marquis', 'matrix', 'megahertz', 'microwave', 'mnemonic', 'mystify', 'naphtha', 'nightclub', 'nowadays', 'numbskull', 'nymph', 'onyx', 'ovary', 'oxidize', 'oxygen', 'pajama', 'peekaboo', 'phlegm', 'pixel', 'pizazz', 'pneumonia', 'polka', 'pshaw', 'psyche', 'puppy', 'puzzling', 'quartz', 'queue', 'quips', 'quixotic', 'quiz', 'quizzes', 'quorum', 'razzmatazz', 'rhubarb', 'rhythm', 'rickshaw', 'schnapps', 'scratch', 'shiv', 'snazzy', 'sphinx', 'spritz', 'squawk', 'staff', 'strength', 'strengths', 'stretch', 'stronghold', 'stymied', 'subway', 'swivel', 'syndrome', 'thriftless', 'thumbscrew', 'topaz', 'transcript', 'transgress', 'transplant', 'triphthong', 'twelfth', 'twelfths', 'unknown', 'unworthy', 'unzip', 'uptown', 'vaporize', 'vixen', 'vodka', 'voodoo', 'vortex', 'voyeurism', 'walkway', 'waltz', 'wave', 'wavy', 'waxy', 'wellspring', 'wheezy', 'whiskey', 'whizzing', 'whomever', 'wimpy', 'witchcraft', 'wizard', 'woozy', 'wristwatch', 'wyvern', 'xylophone', 'yachtsman', 'yippee', 'yoked', 'youthful', 'yummy', 'zephyr', 'zigzag', 'zigzagging', 'zilch', 'zipper', 'zodiac', 'zombie']
     word = random.choice(words_to_guess)
+    temp = word
     length = len(word)
     count = 0
-    display = '_' + str(length)
+    display = '_' 
     already_guessed = []
     play_game = ""
+    hangman()
 def play_loop():
     global play_game
     play_game = input("Do you want to play again ? Y/y = yes, N/n = no \n")
     while play_game not in ["y","Y","n","N"]:
         play_game = input("Do you want to play again ? Y/y = yes, N/n = no \n")
-    if play_game == "y":
+    if play_game == "y" or play_game == 'Y':
         main()
-    elif play_game == "no":
+    elif play_game == "n" or play_game == 'N':
         print("Thanks for Playing! Get OUT Now!")
         exit()
 def hangman():
-    global count,display,word,already_guessed,play_game
+    global count,display,word,already_guessed,play_game,temp,length
     limit = 5
-    guess = input("This is the Hangman Word : " + display + "Enter your guess : ")
+    guess = input("This is the Hangman Word : " + display + str(length) + "\n Enter your guess : ")
     guess = guess.strip()
     if len(guess.strip()) == 0 or len(guess.strip()) >= 2 or guess <= "9":
         print("Invalid Input,Try a letter\n")
         hangman()
     elif guess in word:
-        already_guessed.extend([guess])
-        index = word.find(guess)
-        word = word[:index] + "_" + word[index + 1:]
-        display = display[:index] + guess + display[index + 1:]
-        print(display + "\n")
+        already_guessed.append([guess])
+        index = [i for i, a in enumerate(word) if a == guess]
+        for i in index:
+            word = word[:i] + "_" + word[i + 1:]
+            display = display[:i] + guess + display[i + 1:]
+            print(display + "\n")
     elif guess in already_guessed:
         print("Try another letter.\n")
     else:
@@ -93,17 +96,17 @@ def hangman():
                   "  |    / \\ \n"
                   "__|__ \n")
             print("wrong guss. You are hanged!!!\n")
-            print("The word was : ",already_guessed,word)
+            print("The word was : " + temp + " Your Guesses : ")
+            print(already_guessed, end=",")
             play_loop()
 
-        if word == '_'*length:
-            print("Congrats! You guessed the word right!")
-            play_loop()
-        elif count != limit:
-            hangman()
+    if word == '_'*length:
+        print("Congrats! You guessed the word right!")
+        play_loop()
+    elif count != limit:
+        hangman()
+
     main()
-
-    hangman()
 
 print("\nWelcome to Hangman game\n")
 name = input("Enter your name : ")
